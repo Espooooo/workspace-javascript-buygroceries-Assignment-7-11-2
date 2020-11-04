85438b7be08616d1fd045bdcca1bfb615d08d8fe
Espooooo <espo_brendon@yahoo.com>
2020-11-04 13:15:42 -0500

    11/4 - Assignment 7

--- js/add-element.js
+++ js/add-element.js
@@ -8,21 +8,40 @@ $('li').addClass('cool');
 
 // traverse the elements
 
-
+$('li').click(changeStyle);
 // add a new element
-
+function addElement(){
+  //
+}
 
 // add by clicking the plus sign
+$('add').click(addElement);
+
 document.getElementById("add").addEventListener("click", addElement);
 
 function addElement() {
+  $("#todo").append("<li><input type='text'><li>");
+
   // add a new element
-  
+  $('input').blur(function(){
+    console.log("reach");
+    $(this).parent().addClass("cool");
+
+    var userinput = $(this).val();
+    $(this).parent().text(userinput);
+
+  });
 }
 
 
 //  click the li element will change the changeStyle
 function changeStyle() {
+  if($(this).hasClass('complete')){
+    $(this).removeClass('complete');
+    $(this).addClass('cool');
+  }else{
+    $(this).addClass('complete')
+  }
 
 }
 
